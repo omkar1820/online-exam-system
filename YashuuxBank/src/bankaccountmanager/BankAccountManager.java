2b97ccd54d36a183c7e7733b77517e1f7ca94c56
omkar1820 <mahalingeomkar@gmail.com>
2026-05-12 22:49:42 +0530

    Delete YashuuxBank directory

--- YashuuxBank/src/bankaccountmanager/BankAccountManager.java
+++
@@ -1,9 +0,0 @@
-package bankaccountmanager;
-
-public class BankAccountManager {
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> {
-            new MainFrame().setVisible(true);
-        });
-    }
-}
